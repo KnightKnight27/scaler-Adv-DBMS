767996cac255c2cc76a40a182ee2a737af8e4115
Ujjwal Jain <jainujjwal1609@gmail.com>
2026-05-25 21:40:31 +0530

    Remove create_db.py

--- Lab-4/create_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('my_database.db')
-cursor = conn.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS sample(id INTEGER PRIMARY KEY, name TEXT);')
-cursor.execute('DELETE FROM sample;') # clear just in case
-cursor.execute("INSERT INTO sample(name) VALUES ('Alice'), ('Bob'), ('Charlie');")
-conn.commit()
-conn.close()
