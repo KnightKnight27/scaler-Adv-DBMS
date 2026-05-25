34407aa158a0b02c97467b5169bdfb18523100ce
Shreyas S <shreyass1508@gmail.com>
2026-05-25 20:44:55 +0530

    feat: Lab Submission 5

---
+++ Red Black Tree/RedBlackTree.h
@@ -0,0 +1,14 @@
+//
+// Created by laksh on 25/05/2026.
+//
+
+#ifndef SCALER_ADV_DBMS_REDBLACKTREE_H
+#define SCALER_ADV_DBMS_REDBLACKTREE_H
+
+
+class RedBlackTree
+{
+};
+
+
+#endif //SCALER_ADV_DBMS_REDBLACKTREE_H
