c648be2423b8f2c5ab457453baa5c483e5ded490
ayushsingh199 <ayushsingh0319@gmail.com>
2026-06-17 15:26:58 +0530

    Lab 7 submission done - Ayushkumar 10199

---
+++ Lab-07/main.cpp
@@ -0,0 +1,28 @@
+#include "expression_sql.hpp"
+
+using namespace std;
+
+// ADBMS LAB 5
+// Roll No: 24BCS10199
+// Name: Ayushkumar Singh
+
+int main() {
+
+  ExpressionEvaluator evaluator;
+
+  string expression = "(10+5)*2-8/2";
+
+  cout << "Expression: " << expression << endl;
+
+  cout << "Result: " << evaluator.evaluate(expression) << endl;
+
+  vector<Row> students = {
+
+      {1, "Ayush", 19}, {2, "Anushka", 20}, {3, "Rahul", 19}, {4, "Priya", 21}};
+
+  SQLParser parser;
+
+  parser.executeQuery("SELECT name FROM table WHERE age = 19", students);
+
+  return 0;
+}
