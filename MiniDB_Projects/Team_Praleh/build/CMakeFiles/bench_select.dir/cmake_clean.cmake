9bd82b652bb24d1f33ca1e8d86d3580e3bad438b
Abhay Singh Bhadauria <abhaybhadauria0029@gmail.com>
2026-06-23 21:17:47 +0530

    final project

---
+++ MiniDB_Projects/Team_Praleh/build/CMakeFiles/bench_select.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bench_select.dir/link.d"
+  "CMakeFiles/bench_select.dir/benchmarks/bench_select.cpp.o"
+  "CMakeFiles/bench_select.dir/benchmarks/bench_select.cpp.o.d"
+  "bench_select"
+  "bench_select.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bench_select.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
