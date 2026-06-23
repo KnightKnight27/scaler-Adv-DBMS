49ba4b1d00ea30465c6a4d4f656670a253ebf133
Mehul Agarwal <agarwalmehul423@gmail.com>
2026-06-23 23:31:00 +0530

    final commit

--- MiniDB_Projects/Team_Gojo/build_stale_team_stormborns/CMakeFiles/dynamic_persistence_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for dynamic_persistence_test.
-# This may be replaced when dependencies are built.
--- MiniDB_Projects/Team_Gojo/build_stale_team_stormborns/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mehulagarwal/Documents/ADBMS/scaler-Adv-DBMS/MiniDB_Projects/Team_Gojo
-# Build directory: /Users/mehulagarwal/Documents/ADBMS/scaler-Adv-DBMS/MiniDB_Projects/Team_Gojo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(dynamic_persistence "/Users/mehulagarwal/Documents/ADBMS/scaler-Adv-DBMS/MiniDB_Projects/Team_Gojo/build/dynamic_persistence_test")
-set_tests_properties(dynamic_persistence PROPERTIES  _BACKTRACE_TRIPLES "/Users/mehulagarwal/Documents/ADBMS/scaler-Adv-DBMS/MiniDB_Projects/Team_Gojo/CMakeLists.txt;42;add_test;/Users/mehulagarwal/Documents/ADBMS/scaler-Adv-DBMS/MiniDB_Projects/Team_Gojo/CMakeLists.txt;0;")
